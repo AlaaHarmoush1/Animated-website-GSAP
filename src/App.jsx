b85de4bcc6ffcddf55aa20a338b60a72db205b1b
Alaa harmoush <Alaacoding@outlook.com>
2025-11-25 22:43:28 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero/>
+      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
+    </main>
+  )
+}
+
+export default App
