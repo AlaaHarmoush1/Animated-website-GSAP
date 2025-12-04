f8b55401fb08a0752d73ba6c4f3325d907944eea
Alaa harmoush <Alaacoding@outlook.com>
2025-12-03 23:15:10 -0500

    About Section Done: \:

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero/>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
-      <h1 className='text-7xl text-violet-300' style={{ fontFamily: "zentry" }}>Hello My Friend this is Alaa Harmoush </h1>
+      <Atbout/>
     </main>
   )
 }
