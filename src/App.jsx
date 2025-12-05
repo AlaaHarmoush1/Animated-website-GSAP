e4cce449020883076ae14762474f79860c17ce93
Alaa harmoush <Alaacoding@outlook.com>
2025-12-04 23:22:08 -0500

    Features Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
+import Navbar from './components/layout/Navbar'
+import Features from './components/Features/Features'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar/>
       <Hero/>
       <About/>
+      <Features/>
     </main>
   )
 }
